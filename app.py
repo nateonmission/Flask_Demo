919a91d9f210653e1f38675a20135923a6bb3504
J. Nathan Allen <nateonmission@gmail.com>
2019-04-08 10:09:16 -0400

    begin building login

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-import requests, json, datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, g, flash
+from flask_login import LoginManager, login_user, logout_user, login_required
+from passlib.hash import sha256_crypt
 import hashlib
 import random
+import requests
+import json
+import datetime
 
 import forms
+import models
 
 DEBUG = True
 PORT = 8000
@@ -11,6 +16,30 @@ HOST = '127.0.0.1'
 
 app = Flask(__name__)
 app.secret_key = hashlib.sha256(str(random.getrandbits(256)).encode('utf-8')).hexdigest()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(username):
+    try:
+        return models.User.get(models.User.username == username)
+    except models.DoesNotExist:
+        return None
+
+
+@app.before_request
+def before_request():
+    g.db = models.DATABASE
+    g.db.connect()
+
+
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
 
 
 @app.route('/', methods=('GET', 'POST'))
@@ -22,7 +51,20 @@ def home_screen():
 @app.route('/', methods=('GET', 'POST'))
 def register():
     form = forms.Login()
-    return render_template('register', form=form)
+    if form.validate_on_submit():
+        try:
+            user = models.User.get(models.User.email == form.email.data)
+        except models.DoesNotExist:
+            flash("Authentication Error", "Error")
+        else:
+            if sha256_crypt.verify(form.password.data.encode('utf-8'), user.password.encode('utf-8')):
+                login_user(user)
+                flash("You're Logged in!", "Success")
+                return redirect(url_for('index'))
+            else:
+                flash("Authentication Error", "Error")
+    else:
+        return render_template('login.html', form=form)
 
 
 @app.route('/aq')
---
+++ models.py
@@ -0,0 +1,45 @@
+import datetime
+
+from peewee import *
+from flask_login import UserMixin
+from passlib.hash import sha256_crypt
+# from bcrypt import hashpw, gensalt
+
+
+DATABASE = SqliteDatabase('aq.db')
+
+
+class User(UserMixin, Model):
+    user_key = AutoField(unique=True)
+    first_name = CharField(unique=True)
+    last_name = CharField(unique=True)
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField(max_length=100)
+    joined_at = DateTimeField(default=datetime.datetime.now)
+    is_admin = BooleanField(default=False)
+
+    class Meta:
+        database = DATABASE
+        order_by = ('-joined_at',)
+
+    @classmethod
+    def create_user(cls, first_name, last_name, username, email, password, admin=False):
+        try:
+            cls.create(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                password=sha256_crypt.encrypt(password.encode('utf-8')),
+                joined_at=datetime.datetime.now(),
+                is_admin=admin
+            )
+        except IntegrityError:
+            raise ValueError("User Exists")
+
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([User], safe=True)
+    DATABASE.close()
