21a017783c8e05f84d435baf8de5a0f45e6fc8fa
J. Nathan Allen <nateonmission@gmail.com>
2019-04-08 09:00:24 -0400

    added reg schema

--- forms.py
+++ forms.py
@@ -4,6 +4,61 @@ from wtforms.validators import (DataRequired, Regexp, ValidationError,
                                 Email, Length, EqualTo)
 
 
+# Functions for preventing duplicate registrations
+def name_exists(form, field):
+    if User.select().where(User.username == field.data).exists():
+        raise ValidationError('Username is taken')
+
+
+def email_exists(form, field):
+    if User.select().where(User.username == field.data).exists():
+        raise ValidationError('Email is already used')
+
+
+# Form Schemata
+# Login Form on login.html
 class Login(FlaskForm):
     id = StringField("Username: ", validators=[DataRequired()])
     password = StringField("Password: ", validators=[DataRequired()])
+
+
+# Registration Form on registration.html for creating users
+class Register(FlaskForm):
+    first_name = StringField("First Name: ", validators=[
+        DataRequired(),
+        Regexp(
+            r'^[a-zA-Z\s-]+$',
+            message="Names can only contain letter, spaces, and a dash"
+        ),
+        name_exists
+    ])
+    last_name = StringField("Last Name: ", validators=[
+        DataRequired(),
+        Regexp(
+            r'^[a-zA-Z\s-]+$',
+            message="Names can only contain letter, spaces, and a dash"
+        ),
+        name_exists
+    ])
+    username = StringField('Username', validators=[
+            DataRequired(),
+            Regexp(
+                r'^[a-zA-Z0-9_]+$',
+                message=("Username must be 1 word, letters, numbers, and "
+                         "underscores only")
+            ),
+            name_exists
+    ])
+    email = StringField('Email', validators=[
+            DataRequired(),
+            Email(),
+            email_exists
+    ])
+    password = PasswordField('Password: ', validators=[
+            DataRequired(),
+            Length(min=4),
+            EqualTo('password2',"Passwords must match")
+    ])
+    password2 = PasswordField('Confirm Password: ', validators=[
+            DataRequired(),
+    ])
